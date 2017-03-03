0fcfcdbfbd846aa2c60fc220d3e4c26c8fbd99a8
Jimena Pose <jimenabpose@gmail.com>
2017-03-03 18:50:16 -0300

    Some fixes and settings added (hand made color picker to be improved)

--- app/src/main/java/com/jimenabpose/flashlight/FlashlightActivity.java
+++ app/src/main/java/com/jimenabpose/flashlight/FlashlightActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.hardware.camera2.CameraManager;
@@ -11,28 +12,52 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-public class FlashlightActivity extends AppCompatActivity {
+public class FlashlightActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     public static Camera camera = null;
     public static CameraManager cameraManager = null;
     public static String cameraId = null;
+    private View mScreenLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashlight);
-        setupFromPreferences();
+        mScreenLayout = findViewById(R.id.activity_flashlight);
+        setupCamera();
+        setupSharedPreferences();
 
     }
 
-    private void setupFromPreferences() {
+    private void setupCamera() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                cameraId = cameraManager.getCameraIdList()[0];
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(getBaseContext(), "Exception flashLightOff", Toast.LENGTH_SHORT).show();
+            }
+        } else if (camera == null) {
+            camera = Camera.open();
+        }
+    }
+
+    private void setupSharedPreferences() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        if (preferences.getBoolean(getString(R.string.pref_light_on_key), getResources().getBoolean(R.bool.pref_light_on_default))) {
+        setFlashlighOn(preferences.getBoolean(getString(R.string.pref_light_on_key), getResources().getBoolean(R.bool.pref_light_on_default)));
+        preferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    private void setFlashlighOn(boolean flashlightOn) {
+        if (flashlightOn) {
             this.flashLightOn();
         } else {
             this.flashLightOff();
@@ -43,11 +68,8 @@ public class FlashlightActivity extends AppCompatActivity {
         try {
             if (cameraIsSupported()) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-                    cameraId = cameraManager.getCameraIdList()[0];
                     cameraManager.setTorchMode(cameraId, true);
                 } else if (camera == null) {
-                    camera = Camera.open();
                     Parameters p = camera.getParameters();
                     p.setFlashMode(Parameters.FLASH_MODE_TORCH);
                     camera.setParameters(p);
@@ -86,6 +108,16 @@ public class FlashlightActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
+    private void setScreenColor(int color) {
+        mScreenLayout.setBackgroundColor(color);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -102,5 +134,14 @@ public class FlashlightActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.pref_light_on_key))) {
+            setFlashlighOn(sharedPreferences.getBoolean(key, getResources().getBoolean(R.bool.pref_light_on_default)));
+        } else if (key.equals(getString(R.string.pref_screen_color_key))) {
+            setScreenColor(Color.parseColor(sharedPreferences.getString(key, String.valueOf(R.color.white))));
+        }
     }
 }
